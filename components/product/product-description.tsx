e5260ab912712dd64cd245524de096fd704fb36e
santiagozin <santiagozin90@gmail.com>
2025-11-02 20:33:50 -0300

    add changes

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -9,7 +9,7 @@ export function ProductDescription({ product }: { product: Product }) {
   return (
     <>
       <div className="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
-        <h1 className="mb-2 text-center text-2xl md:text-4xl font-medium">{product.title}</h1>
+        <h1 className="mb-2 text-center text-xl md:text-3xl font-medium">{product.title}</h1>
         {isRobot ? (
           <a
             href="https://wa.me/1170645115"
@@ -20,7 +20,7 @@ export function ProductDescription({ product }: { product: Product }) {
             Contáctanos
           </a>
         ) : (
-          <div className="mr-auto w-auto rounded-full bg-primary p-2 text-sm text-white">
+          <div className="mr-auto w-auto rounded-full bg-primary p-2 text-lg text-white">
             <Price
               amount={product.priceRange.maxVariantPrice.amount}
               currencyCode={product.priceRange.maxVariantPrice.currencyCode}
